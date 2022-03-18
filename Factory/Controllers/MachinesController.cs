088acb9f2bab6f01e96cefb26f75a996832a7d73
Will Greenberg <greenberg.will@gmail.com>
2022-03-18 13:15:11 -0700

    add viewbag for engineer count to add engineer method

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -68,6 +68,7 @@ namespace Factory.Controllers
     {
       var thisMachine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
       ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "Name");
+      ViewBag.EngineerCount = _db.Engineers.Count();
       return View(thisMachine);
     }
     [HttpPost]
